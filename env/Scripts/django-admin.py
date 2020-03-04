10bb939b23a1ef9b1ea7f49fd1a0828182d38549
samorenoq <samorenoq@eafit.edu.co>
2020-03-04 15:47:31 -0500

    Proyecto creado con Django

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\santiago\onedrive - universidad eafit\semestre iv\proyecto integrador\taller 1\proyectointegradortaller1\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
