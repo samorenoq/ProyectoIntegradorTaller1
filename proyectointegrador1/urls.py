1051e9b5057665783021bbdd4616f77d953282f7
samorenoq <samorenoq@eafit.edu.co>
2020-03-04 16:58:44 -0500

    Terminado

--- proyectointegrador1/urls.py
+++ proyectointegrador1/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url, include
+from rest_framework import routers
+from temperature import views
+
+router = routers.DefaultRouter()
+router.register('temperature', views.TemperatureViewSet)
 
 urlpatterns = [
+    url('^', include(router.urls)),
     path('admin/', admin.site.urls),
 ]
---
+++ temperature/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+import uuid
+# Create your models here.
+
+class Temperature(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    type = models.CharField(verbose_name='Tipo', max_length=20)
+    value = models.IntegerField(verbose_name='Valor')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
